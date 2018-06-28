144fea335e4b7addf2baf37933c24f1a53522336
Nade Kang <kangnade@gmail.com>
2018-06-28 10:13:56 +0800

    updated script

--- r_wickham/scripts/wickham_r_chapter8_script.R
+++ r_wickham/scripts/wickham_r_chapter8_script.R
@@ -51,25 +51,79 @@ read_delim("file", '|')
 # Unfortunately, it's painful to parse because you need to describe the length of every field. 
 # Readr aims to make it as easy as possible by providing a number of different ways to describe the field structure.
 
+# 4. Sometimes strings in a CSV file contain commas. To prevent them from cuasing problems they need
+# to be surrounded by a quoting character, like " or '. By convention, read_csv() assumes that the quoting
+# character will be ", and if you want to change it you'll need to use read_delim() instead. What arguments do
+# you need to specify to read the following text into a data frame?
+"x,y\n1,'a,b'"
+?read_delim
 
+# Parsing a Vector
+# Numbers: 1. people write numbers differently such as 1.23 (US) and 1,23 (EU)
+# To address this problem:
+parse_double("1.23")
+parse_double("1,23", locale = locale(decimal_mark = ","))
 
+# Numbers: 2. numbers are often surrounded by other characters such as $1000 or 10%
+# parse_number() solves this situation because it ignores non-numeric characters before
+# and after the number. This is particularly useful for currencies and percentages
+# but works to extract numbers embedded in text.
+parse_number("$100")
+parse_number("20%")
+parse_number("it costs $2000")
 
+# Numbers: 3. numbers often contain "grouping" characters to make them easier to read like:
+# 100,000,000 and so on
+# This problem can be addressed by the combination of parse_number() and the locale as parse_number()
+# will ignore the grouping mark:
+# Used in America
+parse_number("$123,456,789")
 
+# Used in many parts of Europe
+parse_number(
+  "123.456.789",
+  locale = locale(grouping_mark = ".")
+)
 
+# Used in Switzerland
+parse_number(
+  "123'456'789",
+  locale = locale(grouping_mark = "'")
+)
 
+# Strings
+# In R, we can get at the underlying representation of a string using charToRaw()
+charToRaw("Nade")
+# ASCII does a great job of representing English characters because it's the American Standard Code
+# for Information Interchange
+# UTF-8 can encode just about every character used by humans today, as well as many extra symbols emoji
 
+# readr uses UTF-8 everywhere. But this fails for data produced by older systems that do not understand UTF-8
+# If this happens, your string looks weird when you print them
+x1 <- "El Ni\xf1o was particularly bad this year"
+x2 <- "\x82\xb1\x82\xf1\x82\xc9\x82\xbf\x82\xcd"
 
+x1
+x2
 
+# To fix this problem, you'll need to specify the encoding in parse_character()
+parse_character(x1, locale = locale(encoding = "Latin1"))
+parse_character(x2, locale = locale(encoding = "Shift-JIS"))
 
+# How do you find the correct encoding? Assume we are unlucky, readr provides guess_encoding()
+#Try:
+guess_encoding(charToRaw(x1))
+guess_encoding(charToRaw(x2))
+parse_character(x2, locale = locale(encoding = "KOI8-R"))
 
+# Factors
+# R uses factors to represent categorical variables that have a known set of possible values
+# Give parse_factor() a vector of known levels to generate a warning whenever an unexpected
+# value is present:
+fruit <- c("apple", "banana")
+parse_factor(c("apple", "banana", "bananana"), levels = fruit)
 
-
-
-
-
-
-
-
+# Dates, Date-Times, and Times
 
 
 

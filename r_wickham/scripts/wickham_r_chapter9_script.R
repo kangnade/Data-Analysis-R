aca486ae14029a28059fc0b68f1fc00784c29d1a
Nade Kang <kangnade@gmail.com>
2018-06-30 08:14:00 +0800

    Added new script

---
+++ r_wickham/scripts/wickham_r_chapter9_script.R
@@ -0,0 +1,46 @@
+### Chapter 9 Tidy Data with tidyr
+
+### LOAD PACKAGE ###
+library(tidyverse)
+
+### CONTENT ###
+# Two common problems in tidying data
+# 1. One variable might be spread across multiple columns
+# 2. One observation might be scattered across multiple rows
+# I would be really unlucky to encounter both
+# To fix these problems, use tidyr: gather() and spread()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

796c8e3b7be462e108f7b9253d892ecda1c60e5c
Nade Kang <kangnade@gmail.com>
2018-06-30 09:58:07 +0800

    Updated script

--- r_wickham/scripts/wickham_r_chapter9_script.R
+++ r_wickham/scripts/wickham_r_chapter9_script.R
@@ -47,18 +47,87 @@ spread(table2, key = type, value = count)
 # The key and value arguments, spread() and gather() are complements. gather() makes wide
 # tables narrower and longer; spread() makes long tables shorter and wider
 
+# Exercises:
+# 1. Why are gather() and spread() not perfectly symmetrical?
+# Carefully consider the following example:
+stocks <- tibble(
+  year   = c(2015, 2015, 2016, 2016),
+  half  = c(   1,    2,     1,    2),
+  return = c(1.88, 0.59, 0.92, 0.17)
+)
+stocks
+stocks %>% 
+  spread(year, return) %>% 
+  gather("year", "return", `2015`:`2016`)
 
+# The functions spread and gather are not perfectly symmetrical because column type 
+# information is not transferred between them. In the original table the column year 
+# was numeric, but after running spread() and gather() it is a character vector. 
+# This is because variable names are always converted to a character vector by gather().
 
+# 2. Why does this code fail?
 
+table4a %>% 
+  gather(1999, 2000, key = "year", value = "cases")
 
+# Use `1999`, `2000`
 
+# 3. Why does spreading this tibble fail? How could you add a new column to fix the problem?
+people <- tribble(
+  ~name,             ~key,    ~value,
+  #-----------------|--------|------
+  "Phillip Woods",   "age",       45,
+  "Phillip Woods",   "height",   186,
+  "Phillip Woods",   "age",       50,
+  "Jessica Cordero", "age",       37,
+  "Jessica Cordero", "height",   156
+)
 
+people
+# There are two rows with Phillip woods and age.
+people <- tribble(
+  ~name,             ~key,    ~value, ~obs,
+  #-----------------|--------|------|------
+  "Phillip Woods",   "age",       45, 1,
+  "Phillip Woods",   "height",   186, 1,
+  "Phillip Woods",   "age",       50, 2,
+  "Jessica Cordero", "age",       37, 1,
+  "Jessica Cordero", "height",   156, 1
+)
+spread(people, key, value)
 
+# 4. Tidy this simple tibble. Do you need to spread or gather it?
+# What are the variables?
+preg <- tribble(
+  ~pregnant, ~male, ~female,
+  "yes",     NA,    10,
+  "no",      20,    12
+)
+gather(preg, sex, count, male, female)
 
+# Separating and Pull
+# table3 has a different problem: column rate contains two variables (case and population)
+# To fix it, we need separate() function
 
-
-
-
+# Separate
+# separate() pulls apart one column into multiple columns, by splitting wherever a separator
+# character appears. Take table3:
+table3
+# We need to separate the rate variable into cases and populatiohn
+table3 %>%
+  separate(rate, into = c("cases", "population"))
+# By default, separate() will split values wherever it sees a non-alphanumeric character,
+# which isn't a number nor letter. In table3, it splits with /
+# We can rewrite it:
+table3 %>%
+  separate(rate, into = c("case", "population"), sep = "/")
+# By default, separate leaves the column type as it is. Here, both case and population after
+# separation remains as character columns
+# Here, however, character columns are not very useful because they are numbers. We can ask
+# separate() to try and convert to better types using convert = TRUE:
+table3 %>%
+  separate(rate, into = c("case", "population"),
+           convert = TRUE)
 
 
 

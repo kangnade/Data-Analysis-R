7846de9be6f68f3416637988e6a4a4a427075483
Nade Kang <kangnade@gmail.com>
2018-07-01 09:04:34 +0800

    updated

--- r_wickham/scripts/wickham_r_chapter10_script.R
+++ r_wickham/scripts/wickham_r_chapter10_script.R
@@ -52,6 +52,113 @@ weather
 # The variables used to connect each pair of tables are called keys.
 # A key is a variable (or set of variables) that uniquely identifies an observation.
 
+# A primary key uniquely identifies an observation in its own table. For example, 
+# planes$tailnum is a primary key because it uniquely identifies each plane in the planes table.
+
+# A foreign key uniquely identifies an observation in another table. 
+# For example, the flights$tailnum is a foreign key because it appears in the flights table 
+# where it matches each flight to a unique plane.
+
+
+# Once you’ve identified the primary keys in your tables, it’s good practice to verify 
+# that they do indeed uniquely identify each observation. One way to do that is to count() 
+# the primary keys and look for entries where n is greater than one:
+
+planes %>%
+  count(tailnum) %>%
+  filter(n > 1)
+
+# same applies to weather table
+weather %>%
+  count(year, month, day, hour, origin) %>%
+  filter(n > 1)
+
+# In flights table, data plus flight or date plus tailnum are not primary keys
+flights %>%
+  count(year, month, day, flight) %>%
+  filter(n > 1)
+
+flights %>%
+  count(year, month, day, tailnum) %>%
+  filter(n > 1)
+
+# It appears that each flight number flies more than once per day
+# If a table lacks a primary key, use mutate() and two_number() to add one
+
+# A primary key and the corresponding foreign key in another table form a relation
+
+# Exercises
+# 1. Add a surrogate key to flights
+# Use arrange and then use row_number() as flight_id
+flights %>%
+  arrange(year, month, day, sched_dep_time, carrier, flight) %>%
+  mutate(flight_id = row_number()) %>%
+  glimpse()
+
+# 2. Identify the keys in the following datasets:
+# 2-1. Lahman::Batting
+Lahman::Batting
+# Check playerID year ID and stint
+Lahman::Batting %>%
+  count(playerID, yearID, stint) %>%
+  filter(n > 1)
+
+# 2-2 babynames::babynames
+# No data
+
+# 2-3 nasaweather::atmos
+# No data
+
+# 2-4 fueleconomy:vehicles
+# No data
+
+# 2-5 ggplot2::diamonds
+ggplot2::diamonds
+# Check the distinct rows in diamonds
+ggplot2::diamonds %>% 
+  distinct() %>%
+  nrow()
+# Check the total rows
+ggplot2::diamonds %>%
+  nrow()
+# No unique key
+
+# Mutating Joins
+# A mutating join allows you to combine variables from two tables. It first matches observations by their keys, 
+# then copies across variables from one table to the other.
+# Like mutate(), the join functions add variables to the right
+
+flights2 <- flights %>%
+  select(year:day, hour, origin, dest, tailnum, carrier)
+flights2
+
+# If you want to add full airline name to flights2 data, you can combine the airlines and flights2 with left_join()
+
+flights2 %>%
+  select(-origin, -dest) %>%   # get rid of origin and dest
+  left_join(airlines, by = "carrier")
+
+# In R's base language setting:
+flights2 %>%
+  select(-origin, -dest) %>%
+  mutate(name = airlines$name[match(carrier, airlines$carrier)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
